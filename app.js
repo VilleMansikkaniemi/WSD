e8b0f3ea2ba1c7e37e9ff35459bdb1c38551113d
VilleMansikkaniemi <73139444+VilleMansikkaniemi@users.noreply.github.com>
2024-04-07 22:00:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 import { Hono } from "https://deno.land/x/hono@v3.7.4/mod.ts";
+import * as feedbacks from "./feedbacks.js";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hi again!"));
+app.get("/",  (c) => {
+    
+    return c.text("Hello world!");
+  });
+
+app.get("/feedbacks/1", async (c) => {
+    const count = await feedbacks.getFeedbacks(1);
+    return c.text(`Feedback 1: ${count}`);
+});
+app.get("/feedbacks/2", async (c) => {
+    const count = await feedbacks.getFeedbacks(2);
+    return c.text(`Feedback 2: ${count}`);
+});
+app.get("/feedbacks/3", async (c) => {
+    const count = await feedbacks.getFeedbacks(3);
+    return c.text(`Feedback 3: ${count}`);
+});
+
+
+app.post("/feedbacks/1 ", async (c) => {
+    await feedbacks.incFeedbacks(1);
+});
+app.post("/feedbacks/2 ", async (c) => {
+    await feedbacks.incFeedbacks(2);  
+});
+app.post("/feedbacks/3 ", async (c) => {
+    await feedbacks.incFeedbacks(3);
+});
+
 
 export default app;
