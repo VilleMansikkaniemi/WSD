27a738859b0b39a447df0bfff90f644d1dc30650
VilleMansikkaniemi <73139444+VilleMansikkaniemi@users.noreply.github.com>
2024-04-07 22:16:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ import * as feedbacks from "./feedbacks.js";
 
 const app = new Hono();
 
-app.get("/",  (c) => {
-    
-    return c.text("");
-  });
 
 app.get("/feedbacks/1", async (c) => {
     const count = await feedbacks.getFeedbacks(1);
@@ -22,13 +18,13 @@ app.get("/feedbacks/3", async (c) => {
 });
 
 
-app.post("/feedbacks/1 ", async (c) => {
+app.post("/feedbacks/1", async (c) => {
     await feedbacks.incFeedbacks(1);
 });
-app.post("/feedbacks/2 ", async (c) => {
+app.post("/feedbacks/2", async (c) => {
     await feedbacks.incFeedbacks(2);  
 });
-app.post("/feedbacks/3 ", async (c) => {
+app.post("/feedbacks/3", async (c) => {
     await feedbacks.incFeedbacks(3);
 });
 
